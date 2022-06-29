24fb92c65fb8fbaa87983170b1252a73180cea11
Cem Gurhan <cemg@live.co.uk>
2022-06-29 14:31:17 +0100

    Adding endpoints to organisation

--- src/main/java/com/example/demo/controllers/OrganisationController.java
+++ src/main/java/com/example/demo/controllers/OrganisationController.java
@@ -1,13 +1,20 @@
 package com.example.demo.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.models.Organisation;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 //@RequestMapping("/organisation")
 @CrossOrigin(origins = "http://localhost:3000")
 public class OrganisationController {
 
+
+    @GetMapping("findOrganisationByID/{id}")
+    public Organisation findOrganisationByID(@PathVariable("id") Long id){
+
+
+
+
+    }
 
 }
--- src/main/java/com/example/demo/repositories/OrganisationRepo.java
+++ src/main/java/com/example/demo/repositories/OrganisationRepo.java
@@ -1,4 +1,14 @@
 package com.example.demo.repositories;
 
-public interface OrganisationRepo {
+import com.example.demo.models.Organisation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrganisationRepo extends JpaRepository<Organisation,Long> {
+
+    @Query(value = "SELECT * FROM organisations WHERE id = ?",nativeQuery = true)
+    Organisation findOrganisationByID(Long id);
+
 }
