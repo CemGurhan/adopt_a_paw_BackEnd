00b5214c40b315f11e125b9e7ff2296fb28e9285
Edward Todd <edwardtdd@gmail.com>
2022-06-29 22:44:04 +0100

    a

--- src/main/java/com/example/demo/controllers/AnimalController.java
+++ src/main/java/com/example/demo/controllers/AnimalController.java
@@ -4,6 +4,7 @@ import com.example.demo.models.Animal;
 import com.example.demo.models.SexEnums;
 import com.example.demo.repositories.AnimalRepo;
 import com.example.demo.services.AnimalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
