11e08833cd47dbf7cf0ef1c8ba691ffe85ced471
Cem Gurhan <cemg@live.co.uk>
2022-07-05 23:46:11 +0100

    added auth

---
+++ src/main/java/com/example/demo/registration/RegistrationController.java
@@ -0,0 +1,27 @@
+package com.example.demo.registration;
+
+import com.example.demo.email.EmailService;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+
+
+
+    private final RegistrationService registrationService;
+
+    @PostMapping
+    public String register(@RequestBody RegistrationRequest request) {
+        return registrationService.register(request);
+    }
+
+    @GetMapping(path = "confirm")
+    public String confirm(@RequestParam("token") String token) {
+        return registrationService.confirmToken(token);
+    }
+}
